af8ffe3c33ae3d2cc6b718b7be7afe67e760c788
Lucas Azevedo Costa <eozecos@gmail.com>
2025-02-03 23:09:56 -0300

    :bricks: ci: Criação e implementação do Dockerfile

---
+++ src/Database/database.py
@@ -0,0 +1,16 @@
+# database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Configuração do banco de dados
+DATABASE_URL = "postgresql://usuario:senha@db:5434/api_ecommerce"
+
+# Cria a engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+# Cria uma sessão local para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para os modelos
+Base = declarative_base()
--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from Database.database import SessionLocal
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    db = SessionLocal()
+    db.close()
     return "hello world"
 
 if __name__ == "__main__":
